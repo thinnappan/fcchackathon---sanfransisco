aa688216cee6a125bc6f61235856627992269042
Thinnappan.K <thinnappanmsc@gmail.com>
2018-10-13 10:36:32 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,10 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi I am dinesh from India.</h1>
-    <p>I am a Student.</p>   
+    <h1>Hi.</h1>
+    <p>I am a professor.</p>   
     <p>I love hackathons and FCC.</p>	
     <p>I Love coding.</p>
-	<p> This is my personal <a href='http://www.dineshmax.ga' target='_blank'>Portfolio</a>. </p>
 	<br></br>
     <b> <i> Thankyou !!! See you all in online... </i> </b>
   </div>
